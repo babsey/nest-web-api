277335be28bc838d78e70865a9d138acc915becd
Sebastian Spreizer <spreizer@web.de>
2018-03-19 11:30:53 +0100

    180319-2

---
+++ examples/nest_help.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# JSON format
+curl -H "Content-Type: application/json" -d '{"obj": "iaf_psc_alpha", "return_text": "true"}' $NEST_API/help
+
+# POST request
+curl -d "obj=iaf_psc_alpha&return_text=true" $NEST_API/help
+
+# GET request
+curl "$NEST_API/help?obj=iaf_psc_alpha&return_text=true"
+
+# Print only help text, jq is required
+curl -s "$NEST_API/help?obj=iaf_psc_alpha&return_text=1" | jq -r '.response'
--- examples/print_network.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl $NEST_API/PrintNetwork
-curl -H "Content-Type: application/json" -d '{"depth": 2}' $NEST_API/PrintNetwork
